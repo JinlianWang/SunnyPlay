1e89defeacfc9918b7fcdcdee0fefd7e8281bf50
Sunny Wang <wangjinlian@hotmail.com>
2019-01-12 11:29:01 -0500

    Initial Commit

---
+++ SunnyPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SunnyPlay
+//
+//  Created by Sunny Wang on 1/12/19.
+//  Copyright © 2019 Sunny Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
